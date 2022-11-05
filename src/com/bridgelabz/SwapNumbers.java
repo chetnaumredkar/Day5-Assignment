de57fa04aa25b5aae678b078465845e72a396646
kolhechetna <kolhe.chetna@gmail.com>
2022-11-05 09:16:57 +0530

    Added:Program to Compute Quotient and Remainder

---
+++ src/com/bridgelabz/SwapNumbers.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class SwapNumbers {
+public static void main(String[] args) {
+	int a=5;
+	int b=8;
+	System.out.println("the value  of a is  "+a+ " the value of b is "+b);
+	System.out.println("After Swapping");
+	int temp=0;
+	temp=a;
+	a=b;
+	b=temp;
+	System.out.println("the value  of a is  "+a+ " the value of b is "+b);
+
+	
+}
+}
